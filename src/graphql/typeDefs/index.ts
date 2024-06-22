54aa39a3c76b08252382f9352f0897fc2b558339
mdtalha109 <mdtalha1041@gmail.com>
2024-06-22 20:09:35 +0530

    merged expense typedef

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -1,5 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
 import { userTypeDef } from './user.typedef';
+import { expenseTypeDef } from './expense.typedef';
 
-export const mergedTypeDef = mergeTypeDefs([userTypeDef]);
+export const mergedTypeDef = mergeTypeDefs([userTypeDef, expenseTypeDef]);
