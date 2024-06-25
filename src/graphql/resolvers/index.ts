6c598c8604ca927db4effab7a410c402f5b84513
mdtalha109 <mdtalha1041@gmail.com>
2024-06-25 11:53:24 +0530

    feat: merged expense resolver with root resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { mergeResolvers } from '@graphql-tools/merge';
 import { userResolvers } from './userResolvers';
+import expenseResolvers from './expenseResolver';
 
-const mergedResolvers = mergeResolvers([userResolvers]);
+const mergedResolvers = mergeResolvers([userResolvers, expenseResolvers]);
 
 export { mergedResolvers };
