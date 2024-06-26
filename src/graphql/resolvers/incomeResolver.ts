ecceca32b14aa193c6c36191fd4f207f925f9b9e
mdtalha109 <mdtalha1041@gmail.com>
2024-06-26 23:19:01 +0530

    fix(lint): fixed linting issue

--- src/graphql/resolvers/incomeResolver.ts
+++ src/graphql/resolvers/incomeResolver.ts
@@ -6,59 +6,79 @@ import { Context } from '../../types/middleware';
 import { BaseError } from '../../errors/BaseError';
 import { IncomeRepository } from '../../repositories/incomeRepository';
 
-
 const incomeRepository = new IncomeRepository(prisma);
 const incomeService = new IncomeService({ incomeRepository });
 
 const incomeResolvers: IResolvers = {
-    Query: {
-        getIncomes: withAuth(async (_, __, context: Context) => {
-            try {
-                return await incomeService.getIncomes(context.userId!);
-            } catch (error) {
-                handleError(error);
-            }
-        }),
+  Query: {
+    getIncomes: withAuth(async (_, __, context: Context) => {
+      try {
+        return await incomeService.getIncomes(context.userId!);
+      } catch (error) {
+        handleError(error);
+      }
+    }),
 
-        getIncomeById: withAuth(async (_, { id }, context: Context) => {
-            try {
-                return await incomeService.getIncomeById(context.userId!, Number(id));
-            } catch (error) {
-                handleError(error);
-            }
-        }),
-    },
-    Mutation: {
-        addIncome: withAuth(async (_, { description, amount, date, category }, context: Context) => {
-            try {
-                return await incomeService.addIncome(context.userId!, description, amount, new Date(date), category);
-            } catch (error) {
-                handleError(error);
-            }
-        }),
-        updateIncome: withAuth(async (_, { id, description, amount, date, category }, context: Context) => {
-            try {
-                return await incomeService.updateIncome(context.userId!, Number(id), description, amount, date ? new Date(date) : undefined, category);
-            } catch (error) {
-                handleError(error);
-            }
-        }),
-        deleteIncome: withAuth(async (_, { id }, context: Context) => {
-            try {
-                return await incomeService.deleteIncome(context.userId!, Number(id));
-            } catch (error) {
-                handleError(error);
-            }
-        }),
-    },
+    getIncomeById: withAuth(async (_, { id }, context: Context) => {
+      try {
+        return await incomeService.getIncomeById(context.userId!, Number(id));
+      } catch (error) {
+        handleError(error);
+      }
+    }),
+  },
+  Mutation: {
+    addIncome: withAuth(
+      async (_, { description, amount, date, category }, context: Context) => {
+        try {
+          return await incomeService.addIncome(
+            context.userId!,
+            description,
+            amount,
+            new Date(date),
+            category,
+          );
+        } catch (error) {
+          handleError(error);
+        }
+      },
+    ),
+    updateIncome: withAuth(
+      async (
+        _,
+        { id, description, amount, date, category },
+        context: Context,
+      ) => {
+        try {
+          return await incomeService.updateIncome(
+            context.userId!,
+            Number(id),
+            description,
+            amount,
+            date ? new Date(date) : undefined,
+            category,
+          );
+        } catch (error) {
+          handleError(error);
+        }
+      },
+    ),
+    deleteIncome: withAuth(async (_, { id }, context: Context) => {
+      try {
+        return await incomeService.deleteIncome(context.userId!, Number(id));
+      } catch (error) {
+        handleError(error);
+      }
+    }),
+  },
 };
 
 function handleError(error: any) {
-    if (error instanceof BaseError) {
-        throw new Error(JSON.stringify(error));
-    } else {
-        throw new Error(`Unknown error: ${error.message}`);
-    }
+  if (error instanceof BaseError) {
+    throw new Error(JSON.stringify(error));
+  } else {
+    throw new Error(`Unknown error: ${error.message}`);
+  }
 }
 
 export default incomeResolvers;
